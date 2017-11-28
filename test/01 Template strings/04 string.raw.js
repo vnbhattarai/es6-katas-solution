bbc6448d2552c633e80b1fc1dd6999c36fd059b5
Vinaya <vnbhattarai@protonmail.ch>
2017-11-28 20:08:00 +0200

    style: 💄 Change indenting and format of template-strings katas

--- test/01 Template strings/04 string.raw.js
+++ test/01 Template strings/04 string.raw.js
@@ -3,36 +3,36 @@
 const assert = require("assert");
 
 describe("on tagged template strings you can use the `raw` property like so `s.raw`", function() {
-	it("the `raw` property accesses the string as it was entered", function() {
-		function firstChar(strings) {
-			return strings;
-		}
-		assert.equal(firstChar`\\n`, "\\n");
-	});
+  it("the `raw` property accesses the string as it was entered", function() {
+    function firstChar(strings) {
+      return strings;
+    }
+    assert.equal(firstChar`\\n`, "\\n");
+  });
 
-	it("`raw` can access the backslash of a line-break", function() {
-		function firstCharEntered(strings) {
-			var lineBreak = strings.raw[0];
-			return lineBreak[0];
-		}
-		assert.equal(firstCharEntered`\n`, "\\");
-	});
+  it("`raw` can access the backslash of a line-break", function() {
+    function firstCharEntered(strings) {
+      var lineBreak = strings.raw[0];
+      return lineBreak[0];
+    }
+    assert.equal(firstCharEntered`\n`, "\\");
+  });
 
-	describe("`String.raw` as a static function", function() {
-		it("concats the raw strings", function() {
-			var expected = "\\n";
-			assert.equal(String.raw`\n`, expected);
-		});
+  describe("`String.raw` as a static function", function() {
+    it("concats the raw strings", function() {
+      var expected = "\\n";
+      assert.equal(String.raw`\n`, expected);
+    });
 
-		it("two raw-templates-string-backslashes equal two escaped backslashes", function() {
-			const TWO_BACKSLASHES = "\\\\";
-			assert.equal(String.raw`\\`, TWO_BACKSLASHES);
-		});
+    it("two raw-templates-string-backslashes equal two escaped backslashes", function() {
+      const TWO_BACKSLASHES = "\\\\";
+      assert.equal(String.raw`\\`, TWO_BACKSLASHES);
+    });
 
-		it("works on unicodes too", function() {
-			var smilie = "\\u{1F600}";
-			var actual = String.raw`\u{1F600}`;
-			assert.equal(actual, smilie);
-		});
-	});
+    it("works on unicodes too", function() {
+      var smilie = "\\u{1F600}";
+      var actual = String.raw`\u{1F600}`;
+      assert.equal(actual, smilie);
+    });
+  });
 });
